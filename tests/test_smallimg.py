f8dd46bfbec90ed7433026b5e6627e881d985d62
Frits Sweijen <tikk3r@users.noreply.github.com>
2021-12-01 14:59:35 +0100

    Change test scripts

--- tests/test_smallimg.py
+++ tests/test_smallimg.py
@@ -1,4 +1,5 @@
 import legacystamps
 
-print('Easy small cutout')
-legacystamps.download(ra=154.7709, dec=46.4537, bands='grz', mode='jpeg', size=0.01, layer='ls-dr9')
+def test_smallimg():
+    print('Easy small cutout')
+    legacystamps.download(ra=154.7709, dec=46.4537, bands='grz', mode='jpeg', size=0.01, layer='ls-dr9')
