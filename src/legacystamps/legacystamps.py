eabda386ef97cf6d4b4beb2a9469110423e6c6dc
Frits Sweijen <tikk3r@users.noreply.github.com>
2022-05-12 12:06:42 +0200

    Fix argparse error

--- src/legacystamps/legacystamps.py
+++ src/legacystamps/legacystamps.py
@@ -147,7 +147,7 @@ if __name__ == '__main__':
     parser.add_argument('--mode', type=str, required=False, default='jpeg', help='Image type to retrieve. Can be "jpeg", "fits" or "both" to retrieve either a JPEG image, FITS file or both. Default value is jpeg.')
     parser.add_argument('--size', type=float, required=False, default=0.01, help='Cutout size in degrees.')
     parser.add_argument('--layer', type=str, required=False, default='ls-dr9', help='Layer to make a cutout from. Default value is ls-dr9. Examples are ls-dr9, sdss or unwise-neo4. See Legacy documentation for all possibilies.')
-    parser.add_argument('--autoscale', type=bool, required=False, default=False, dest='autoscale', action='store_true', help='Automatically change the pixel size if the resulting image would exceed the server maximum of 3000x3000 pixels.')
+    parser.add_argument('--autoscale', required=False, default=False, dest='autoscale', action='store_true', help='Automatically change the pixel size if the resulting image would exceed the server maximum of 3000x3000 pixels.')
     parser.add_argument('--download-dir', type=str, required=False, default='', dest='ddir', help='Directory to store downloaded files. If not given will download to $PWD.')
     args = parser.parse_args()
     download(args.ra, args.dec, args.bands, mode=args.mode, size=args.size, layer=args.layer, autoscale=args.autoscale, ddir=args.ddir)
